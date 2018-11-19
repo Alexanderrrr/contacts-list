83cf932197928e85bdf1bb052d9755808f141ad6
Alexanderrrr <acons1987@gmail.com>
2018-11-19 21:58:36 +0100

    Table insert and delete

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,12 +1,27 @@
 <template>
 <div>
+  <h3>Add contact</h3>
+  <form @submit.prevent="addContact">
+    <label>First Name</label>
+    <input v-model="newContact.firstName" placeholder="first name"><br>
+
+    <label>Last Name</label>
+    <input v-model="newContact.lastName" placeholder="last name"><br>
+
+    <label>Email</label>
+    <input v-model="newContact.email" placeholder="email"><hr>
+
+    <button type="submit">ADD CONTACT</button>
+  </form>
   <h3>Contact List</h3>
+
   <table border=1>
     <thead>
       <th>KEY</th>
       <th>Firt Name</th>
       <th>Last Name</th>
       <th>email</th>
+      <th></th>
     </thead>
     <tbody>
       <tr v-for="(contact, index) in contacts" :key="index">
@@ -14,6 +29,9 @@
         <td>{{ contact.firstName }}</td>
         <td>{{ contact.lastName }}</td>
         <td>{{ contact.email }}</td>
+        <td>
+          <button @click='removeContact(contact)'>Remove</button>
+        </td>
       </tr>
     </tbody>
   </table>
@@ -23,25 +41,44 @@
 
 <script>
 export default{
+
   data() {
     return{
+      newContact: {
+        firstName: '',
+        lastName: '',
+        email: '',
+      },
       contacts: [
-
         {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
         {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
         {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
         {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
         {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
         {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
-
-
       ]
     }
-  }
+  },
+
+  methods: {
+    addContact() {
+      this.contacts.push(this.newContact);
+      this.newContact = {};
+    },
+
+    removeContact(contact){
+      let index = this.contacts.indexOf(contact);
+      this.contacts.splice(index, 1);
+      console.log(index);
+    }
+  },
 }
 </script>
 
 <style scoped>
+template {
+  text-align: center;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -56,4 +93,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
