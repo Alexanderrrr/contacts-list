f6b1fd6e77a82e7a59f4d820a8a2dce8ed937269
Alexanderrrr <acons1987@gmail.com>
2018-11-19 21:01:53 +0100

    Table import

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,7 +1,23 @@
 <template>
-
+<div>
   <h3>Contact List</h3>
-  
+  <table border=1>
+    <thead>
+      <th>KEY</th>
+      <th>Firt Name</th>
+      <th>Last Name</th>
+      <th>email</th>
+    </thead>
+    <tbody>
+      <tr v-for="(contact, index) in contacts" :key="index">
+        <td>{{ index }}</td>
+        <td>{{ contact.firstName }}</td>
+        <td>{{ contact.lastName }}</td>
+        <td>{{ contact.email }}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 </template>
 
 
@@ -9,8 +25,35 @@
 export default{
   data() {
     return{
-      contacts: []
+      contacts: [
+
+        {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
+        {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
+        {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
+        {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
+        {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
+        {firstName: "Saban",lastName: "Saulic",email: "saban@saulic.grand"},
+
+
+      ]
     }
   }
 }
 </script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
